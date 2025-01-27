8a00ef121dedca4be2dd4428394b32caf118886b
JonusBurger <105583255+JonusBurger@users.noreply.github.com>
2025-01-27 06:56:25 +0100

    Write pseudocode

---
+++ script.js
@@ -0,0 +1,11 @@
+// Create Object for Game
+
+// Create Gamefield and function for resetting
+
+// Create function for accessing gamestate
+
+// Create function for placing a marker
+
+// Create function to check if placing is valid
+
+// Create function to check if a game is won
