76ab1a7f3eb4b20696d0f65faba389b4c91bf3ed
JonusBurger <j.bergner@live.de>
2025-01-29 15:14:54 +0100

    start building DOM with JS

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 // Create Object for Game
-function ticTacToe () {
+const ticTacToe = (function () {
     // Create Gamefield and function for resetting
     const xLength = 3;
     const yLength = 3;
@@ -24,12 +24,12 @@ function ticTacToe () {
     }
 
     // Create function for placing a marker
-    function placeMarker(token){
+    function placeMarker(x, y, token){
         // Create function to check if placing is valid
-        const x = parseInt(prompt("Enter x_pos:"));
-        const y = parseInt(prompt("Enter y_pos:"));
-        if ((x < 0 || x >= xLength) || (y < 0 || y >= yLength) || field[x][y] != undefined) {
+        if ((x < 0 || x >= xLength) || (y < 0 || y >= yLength)) {
             console.log("Not allowed! please enter valide field")
+        } else if (field[x][y] != undefined) {
+            console.log("Marker already there!")   
         } else {
         field[x][y] = token;
         console.log("Token placed!");
@@ -39,20 +39,20 @@ function ticTacToe () {
     // Create function to check if a game is won
     function checkWinner() {
         for (let i = 0; i < xLength; i++){
-            if (field[i][0] === field[i][1] == field[i][2] && field[i][2] != undefined) {
+            if ((field[i][0] === field[i][1] && field[i][0] === field[i][2]) && (field[i][2] != undefined)) {
                 console.log("Game Over! A Player has won");
                 return field[i][0]
             }
-            if (field[0][i] === field[1][i] == field[2][i] && field[2][i] != undefined) {
+            if ((field[0][i] === field[1][i] && field[0][i] === field[2][i]) && (field[2][i] != undefined)) {
                 console.log("Game Over! A Player has won");
                 return field[0][i]
             }
         }
-        if (field[0][0] === field[1][1] == field[2][2] && field[2][2] != undefined) {
+        if ((field[0][0] === field[1][1] && field[0][0] === field[2][2]) && (field[2][2] != undefined)) {
             console.log("Game Over! A Player has won");
             return field[0][0]
         }    
-        if (field[2][0] === field[1][1] == field[0][2] && field[0][2] != undefined) {
+        if ((field[2][0] === field[1][1] && field[2][0] === field[0][2]) && (field[0][2] != undefined)) {
             console.log("Game Over! A Player has won");
             return field[2][0]
         }    
@@ -73,7 +73,7 @@ function ticTacToe () {
         return true 
     }
     return { createField, getGameState, placeMarker, checkWinner, checkTie }
-}
+})();
 
 // Spieler Objekt benötigt
 function player(name, marker = "o") {
@@ -102,30 +102,86 @@ function player(name, marker = "o") {
     return { playerName, getMarker, updateScore, getScore, resetScore }
 }
 
-function playGame() {
-    let namePlayer1 = prompt("Enter Name of Player 1:");
-    let namePlayer2 = prompt("Enter Name of Player 2:");
+// Function um das Spielfeld zu rendern
+(function () {
+    // document abgreifen
+    const mainDiv = document.getElementById("mainDiv");
+    // abfragen ob es element in html schon gibt
+    function buildGameField() {
+        const gameFieldDiv = document.createElement("div");
+        gameFieldDiv.classList.add("gameFieldDiv");
+        const xLength = 3;
+        const yLength = 3;
+        gameFieldDiv.active = false;
+    
+        for (let i = 0; i < xLength; i++){
+            const gameFieldRow = document.createElement("div");
+            gameFieldRow.classList.add("row");
+            gameFieldRow.classList.add(`number${i}`);
+            for (let j = 0; j < yLength; j++){
+                const gameFieldCell = document.createElement("div");
+                gameFieldCell.textContent = "";
+                gameFieldCell.classList.add("cell");
+                gameFieldCell.classList.add(`number${j}`);
+                gameFieldRow.appendChild(gameFieldCell);
+            }
+            gameFieldDiv.appendChild(gameFieldRow);
+        console.log("Field was created!");
+        }
+        const btnRestart = document.createElement("button");
+        mainDiv.appendChild(gameFieldDiv);
+        btnRestart.setAttribute("id", "btnRestart");
+        btnRestart.innerText = "Restart";
+        btnRestart.classList.add("btn");
+        btnRestart.addEventListener("click", function() {
+            const nodeList = gameFieldDiv.querySelectorAll(`.cell`);
+            for (node of nodeList) {
+                node.innerText = "";
+            }
+        });
+        mainDiv.appendChild(btnRestart);
+    }
+    // falls nein - bauen des elements
+    if (mainDiv.querySelector("#gameFieldDiv") === null) {
+        buildGameField();
+    }
+})()
 
+
+
+
+
+
+function playGame() {
+    // fetch player data
     const player1 = player(namePlayer1);
     const player2 = player(namePlayer2, "x");
 
-    const game = ticTacToe();
+    // create virtuell field
+    ticTacToe.createField();
 
-    game.createField();
-
-    while (!game.checkTie()) {
-        console.log(game.getGameState());
-        game.placeMarker(player1.getMarker());
+    //setup event Listener
+    const cellList = document.querySelectorAll(".cell");
+    for (cell of cellList) {
+        cell.addEventListener("onclick", function(e) {
+            if (e.currentTarget.innerText === "") {
+                e.currentTarget.innerText = "x";
+                
+            }
+        })
+    }
+{
+        game.placeMarker(x, y, player1.getMarker());
         if (game.checkWinner()) {
             break;
         }
-        game.placeMarker(player2.getMarker());
+        x = prompt("Enter x:");
+        y = prompt("Enter y:");
+        game.placeMarker(x,y, player2.getMarker());
         if (game.checkWinner()) {
             break;
         }
     }
-
-
 }
 
 
